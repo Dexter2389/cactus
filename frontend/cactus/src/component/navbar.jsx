3a93939fcc64569d81b2d25684addd1d9c71dba8
Realmbird <happychristopher777@gmail.com>
2024-06-08 13:38:13 -0700

    Created react frame and added logo

---
+++ frontend/cactus/src/component/navbar.jsx
@@ -0,0 +1,9 @@
+import styles from './style/navbar.module.css'
+const Navbar = () => {
+    return(
+        <nav className={styles.navbar}>
+                        
+        </nav>
+    )
+}
+export default Navbar
